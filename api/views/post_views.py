f2006a89124b80407565af3f0a1a38099741c351
areeshz <azindani786@gmail.com>
2020-08-08 11:44:40 -0400

    Modified views/urls to be RESTful
    Added PostManageView and PostDetailManageView
    Both inherit from BaseManageView class
    ManageViews used to allow multiple views with different authentication and permission classes to share the same route (RESTful)

--- api/views/post_views.py
+++ api/views/post_views.py
@@ -84,3 +84,30 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     else:
       post.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BaseManageView(APIView):
+    """
+    The base class for ManageViews
+        A ManageView is a view which is used to dispatch the requests to the appropriate views
+        This is done so that we can use one URL with different methods (GET, PUT, etc)
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(self, 'VIEWS_BY_METHOD'):
+            raise Exception('VIEWS_BY_METHOD static dictionary variable must be defined on a ManageView class!')
+        if request.method in self.VIEWS_BY_METHOD:
+            return self.VIEWS_BY_METHOD[request.method]()(request, *args, **kwargs)
+
+        return Response(status=405)
+
+class PostsManageView(BaseManageView):
+    VIEWS_BY_METHOD = {
+        'GET': PostsIndex.as_view,
+        'POST': PostsCreate.as_view
+    }
+
+class PostDetailManageView(BaseManageView):
+    VIEWS_BY_METHOD = {
+        'GET': PostsShow.as_view,
+        'PATCH': PostDetail.as_view,
+        'DELETE': PostDetail.as_view
+    }
